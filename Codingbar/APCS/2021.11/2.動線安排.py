8788a40b5ec824bbfd1c987d3ba45fbfee50b309
aw52052001 <aw52052001@gmail.com>
2021-11-27 22:42:54 +0800

    master

--- Codingbar/APCS/2021.11/2.動線安排.py
+++ Codingbar/APCS/2021.11/2.動線安排.py
@@ -1,57 +1,128 @@
 def find1(record,i,j):  #上
-    for x in range(c):
-        print(record,c)
+    for x in range(count):
+        # print("11",record,count)
         if record[x][1] == j and i > record[x][0]:
             return x
     return "NO"
 
 def find2(record,i,j):  #下
-    for x in range(c):
+    for x in range(count):
         if record[x][1] == j and i < record[x][0]:
             return x
     return "NO"
 
 def find3(record,i,j):  #左
-    for x in range(c):
+    for x in range(count):
         if record[x][0] == i and j > record[x][1]:
             return x
     return "NO"
 
 
 def find4(record,i,j):  #右
-    for x in range(c):
+    for x in range(count):
         if record[x][0] == i and j < record[x][1]:
             return x
     return "NO"
 
+def delete(record,i,j): # 刪掉樁
+    for i in range(count):
+        if record[i] == [i,j]:
+            del record[i]
+            break
+
+
+
+def printm():
+    for i in range(m):
+        print(matrix[i])
 
 m,n,h = map(int,input().split())
 
-matrix = [[0]*n for i in range(m)]
-print(matrix)
+matrix = [['0']*n for i in range(m)]
+# print(matrix)
 record = []
 count = 0
+maxv = 0
+now = 0
 for i in range(h):
     r,c,a = map(int,input().split())
     if a == 0:
         record.append([r,c])
         count += 1
         matrix[r][c] = "@"
+
         #上 下 左 右
         x1,x2,y1,y2 = find1(record,r,c),find2(record,r,c),find3(record,r,c),find4(record,r,c)
-        print(x1,x2,y1,y2)
+        # print(x1,x2,y1,y2,c)
         if x1 != "NO":
             for j in range(r-1,record[x1][0],-1):
-                matrix[j][c] = "x"
+                if matrix[j][c] == "0":
+                    matrix[j][c] = str(r)+str(c)
+ 
+        # printm()
         if x2 != "NO":
-            for j in range(r-1,record[x2][0]):
-                matrix[j][c] = "x"
+            for j in range(r+1,record[x2][0]):
+                if matrix[j][c] == "0":
+                    matrix[j][c] = str(r)+str(c)
+ 
+        # printm()
         if y1 != "NO":
-            print("y1",c)
             for j in range(c-1,record[y1][1],-1):
-                matrix[r][j] = "x"
+                if matrix[r][j] == "0":
+                    matrix[r][j] = str(r)+str(c)
+
+        # printm()
         if y2 != "NO":
-            for j in range(c-1,record[y1][1]):
-                matrix[r][j] = "x"
+            for j in range(c+1,record[y2][1]):
+                if matrix[r][j] == "0":
+                    matrix[r][j] = str(r)+str(c)
+
+        maxv = max(now,maxv)
+        # printm()
+        # print(now)
+    elif a == 1:
+        printm()
+        now = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] != "0":
+                    now += 1
+        maxv = max(now,maxv)
+        matrix[r][c] = '0'
+        delete(record,r,c)
+        now -= 1
+        count -= 1
+        x1,x2,y1,y2 = find1(record,r,c),find2(record,r,c),find3(record,r,c),find4(record,r,c)
+        # print(x1,x2,y1,y2,c)
+        if x1 != "NO":
+            for j in range(r-1,record[x1][0],-1):
+                if matrix[j][c] == str(r)+str(c):
+                    matrix[j][c] = '0'
+
+        # printm()
+        if x2 != "NO":
+            for j in range(r+1,record[x2][0]):
+                if matrix[j][c] == str(r)+str(c):
+                    matrix[j][c] = '0'
+ 
+        # printm()
+        if y1 != "NO":
+            for j in range(c-1,record[y1][1],-1):
+                if matrix[i][j] == str(r)+str(c):
+                    matrix[r][j] = '0'
+     
+        # printm()
+        if y2 != "NO":
+            for j in range(c+1,record[y2][1]):
+                if matrix[i][j] == str(r)+str(c):
+                    matrix[r][j] = '0'
+now = 0
 for i in range(m):
-    print(matrix[i])
+    for j in range(n):
+        if matrix[i][j] != "0":
+            now += 1
+printm()
+
+
+print(maxv)
+print(now)
--- Codingbar/L1/分治法/最大連續子陣列.py
+++ Codingbar/L1/分治法/最大連續子陣列.py
@@ -1,23 +1,59 @@
-def subarr(a, le, ri):
-    if le >= ri:
-        return 0
-    if le+1 == ri:
-        return max(a[le], 0)
-    mid = (le + ri) // 2
-    largest = max(subarr(a, le, mid), subarr(a, mid, ri))
+def max_sub_array(i,j,nums):
+    if i == j:
+        return nums[i]
+    else:
+        mid = (i + j) // 2 
+        max_left  = max_sub_array(i,mid,nums)
+        max_right = max_sub_array(mid+1, j,nums)
+    print(i,j)
+    #合併
+    if j - i == 1:
+        maxr = nums[j]
+        maxl = nums[i]
+    else:
+        maxl = nums[(i+j)//2]
+        temp = 0
+        for x in range((i+j)//2 , i-1, -1):
+            temp += nums[x]
+            maxl = max(maxl,temp)
 
-    sum = 0
-    lmax = 0 
-    rmax = 0
-    for i in range(mid-1, le-1, -1):
-        sum += a[i]
-        lmax = max(lmax, sum)
-    sum = 0
-    for i in range(mid, ri):
-        sum += a[i]
-        rmax = max(rmax, sum)
+        maxr = nums[(i+j)//2+1]
+        temp = 0
+        for x in range((i+j)//2+1, j+1):
+            temp += nums[x]
+            maxr = max(maxr,temp)
+    print(max_left, max_right, maxl, maxr)
+    return max(max_left, max_right, maxr+maxl)
+    # n = len(nums)
+    #     #递归终止条件
+    # if n == 1:
+    #     return nums[0]
+    # else:
+    #     #递归计算左半边最大子序和
+    #     max_left = max_sub_array(0,0,nums[0:len(nums) // 2])
+    #     #递归计算右半边最大子序和
+    #     max_right = max_sub_array(0,0,nums[len(nums) // 2:len(nums)])
+    # print(nums)
+    # #计算中间的最大子序和，从右到左计算左边的最大子序和，从左到右计算右边的最大子序和，再相加
+    # max_l = nums[len(nums) // 2 - 1]
+    # tmp = 0
+    # for i in range(len(nums) // 2 - 1, -1, -1):
+    #     tmp += nums[i]
+    #     max_l = max(tmp, max_l)
+    # max_r = nums[len(nums) // 2]
+    # tmp = 0
+    # for i in range(len(nums) // 2, len(nums)):
+    #     tmp += nums[i]
+    #     max_r = max(tmp, max_r)
+    # print(max_right,max_left,max_l,max_r)
+    # #返回三个中的最大值
+    # return max(max_right,max_left,max_l+max_r)
 
-    return max(largest, lmax+rmax)
-n = int(input())
-a = list(map(int,input().split()))
-print(subarr(a, 0, len(a)-1))
+
+
+
+
+
+nums = list(map(int,input().split()))
+nums = [1,2,-1,-2,2,1,-2,1]
+print(max_sub_array(0, len(nums)-1, nums))
