0c2ead10879c7fe18ed317a2e4878c03e150e2ca
aw52052001 <aw52052001@gmail.com>
2021-12-10 22:14:25 +0800

    master

--- Codingbar/L1/分治法/快速排序.py
+++ Codingbar/L1/分治法/快速排序.py
@@ -5,8 +5,9 @@ def quicksort(i, j):
         left = i
         right = j
         print(left,right)
-        print(nums)
+        
         pivot = i
+        print("pivot",pivot)
         i += 1
         while i < j:
             if nums[i] > nums[pivot] and nums[j] < nums[pivot]: 
@@ -20,10 +21,7 @@ def quicksort(i, j):
             i -= 1
         nums[i], nums[pivot] = nums[pivot], nums[i]
         pivot = i 
-    input()
-    print("pivot",pivot)
-    print("after",nums)
-    print(left,right,pivot)
+    print(nums)
     quicksort(left, pivot-1)
     quicksort(pivot+1, right)
     return nums
@@ -34,25 +32,57 @@ nums = [1,454,234,12,6,234,321456,65412,342]
 print(quicksort(0, len(nums)-1))
 
 
-def quick_sort(nums):
-    n = len(nums)
+# def quick_sort(nums):
+#     n = len(nums)
 
-    def quick(left, right):
-        if left >= right:
-            return nums
-        pivot = left
-        i = left
-        j = right
-        while i < j:
-            while i < j and nums[j] > nums[pivot]:
-                j -= 1
-            while i < j and nums[i] <= nums[pivot]:
-                i += 1
-            nums[i], nums[j] = nums[j], nums[i]
-        nums[pivot], nums[j] = nums[j], nums[pivot]
-        quick(left, j - 1)
-        quick(j + 1, right)
-        return nums
+#     def quick(left, right):
+#         if left >= right:
+#             return nums
+#         pivot = left
+#         i = left
+#         j = right
+#         while i < j:
+#             while i < j and nums[j] > nums[pivot]:
+#                 j -= 1
+#             while i < j and nums[i] <= nums[pivot]:
+#                 i += 1
+#             nums[i], nums[j] = nums[j], nums[i]
+#         nums[pivot], nums[j] = nums[j], nums[pivot]
+#         quick(left, j - 1)
+#         quick(j + 1, right)
+#         return nums
 
-    return quick(0, n - 1)
+#     return quick(0, n - 1)
 
+'''
+left = , right =  , pivot = 
+
+left = 0, right = 6 , pivot = 6
++34 +98 +25 +83 +57 +74 +16 (1)
+i = 34
+j = 34
+left = 1, right = 6 , pivot = 6
++16 +98 +25 +83 +57 +74 +34 (2)
+i = 98
+j = 25
++16 +25 +98 +83 +57 +74 +34
++16 +25 +34 +83 +57 +74 +98 (3)
+left = 1, right = 2 , pivot = 2
++16 +25 +34 +83 +57 +74 +98 (4)
+
+left = 3, right =  6, pivot = 6
++16 +25 +34 +83 +57 +74 +98 
++16 +25 +34 +83 +57 +74 +98 (5)
+
+left = 3, right = 5 , pivot = 5 
++16 +25 +34 +83 +57 +74 +98
+i = 83
+j = 57
++16 +25 +34 +57 +83 +74 +98
++16 +25 +34 +57 +74 +83 +98 (6)
+
+left = 3, right = 4 , pivot = 4 (7)
+
+left = 4, right = 5 , pivot = 5 (8)
+
+'''
