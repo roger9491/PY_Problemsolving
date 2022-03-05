f157b3d20e1a463f69c96447930a93c19a40508b
roger9491 <aw52052001@gmail.com>
2022-03-05 22:59:48 +0800

    master

---
+++ Codingbar/APCS/2017.10/4.物品堆疊.py
@@ -0,0 +1,54 @@
+'''
+https://zerojudge.tw/ShowProblem?problemid=c471
+
+from functools import cmp_to_key
+
+sorted(kids, key=cmp_to_key(cmp))
+
+ex2
+總:5
+次:5
+
+        底  頂
+2/2     6   6      
+3/3     6   6
+
+ex2
+總:12
+次:6
+
+        底  頂
+3/1     9   15
+4/2     16  16
+5/3     21  
+
+2 
+20 10 
+1 1 
+'''
+
+from functools import cmp_to_key
+def cmp(x, y):
+    if x[0]*y[1] > y[0]*x[1]:
+        return 1
+    else:
+        return 0
+
+n = int(input())
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+t = []
+for i in range(n):
+    t.append([a[i],b[i]])
+
+t = sorted(t, key=cmp_to_key(cmp))
+
+temp = sum(a)
+ans = 0
+for i in range(n):
+    temp -= t[i][0]
+    ans += temp*t[i][1]
+
+print(ans)
---
+++ Codingbar/APCS/2018.10/4.置物櫃出租.py
@@ -0,0 +1,43 @@
+'''
+https://judge.tcirc.tw/ShowProblem?problemid=d075
+
+演算法: dp (01背包)
+
+
+ex1
+3 10 6  n, m, s
+4 4 1   f
+
+5
+
+5 20 14
+8 2 7 2 1 
+
+15
+'''
+
+n, m, s = map(int,input().split())
+
+f = list(map(int,input().split()))
+
+count = m - s
+ans = sum(f)
+dp = [0]*(count+1)
+
+if ans <= count:
+    print(0)
+else:
+    for i in range(n):
+        if f[i] > count:
+            pass
+        else:
+            for j in range(count,0,-1):
+                if j >= f[i]:
+                    dp[j] = max(dp[j-f[i]] + f[i], dp[j])
+                else:
+                    break
+            # print(dp)
+
+
+    print(ans-dp[-1])
+
