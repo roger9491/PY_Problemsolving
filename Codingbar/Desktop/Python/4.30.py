7280d50a7c512f3bed0c4d2f01a174ab9b3b7bf5
aw52052001 <aw52052001@gmail.com>
2022-04-30 13:39:59 +0800

    master

---
+++ Codingbar/Desktop/Python/4.30.py
@@ -0,0 +1,34 @@
+'''
+雞兔同籠
+
+暴力版
+
+30  隻數
+72  腳數
+
+24 6
+雞 兔子
+'''
+# t = int(input())
+# f = int(input())
+
+# for i in range(1,31): 
+#     r = f - 2*i
+#     # print(r)
+#     if r % 4 == 0:
+#         if  t - i == r // 4:
+#             print(i,t-i)
+#             break
+# else:
+#     print("無解")
+'''
+速解
+'''
+
+# t = int(input())
+# f = int(input())
+
+# if f/2 - t >= 0 and int(f/2 - t) == f/2 - t:
+#     print(int(t - f/2 + t),int(f/2 - t))
+# else:
+#     print("wrong")
--- Codingbar/test1.py
+++ Codingbar/test1.py
@@ -1,24 +1,36 @@
+mem = []
+for i in range(9):
+    mem.append(input().split()[1:])
 
-t = []
+base = [0,0,0,0,0]
 
-for i in range(100):
-    n = input()
-    n = float(n)
-    t.append(n)
+point = 0
+def run(n):
+    global point
+    for i in range(n):
+        base[1],base[2],base[3],base[4] = base[0],base[1],base[2],base[3]
+        base[0] = 0
+        if base[4]:
+            point += 1;base[4] = 0
 
-test = []
-s = -1
-e = 0.1
-for i in range(10):
-    test.append([s,e])
-    if i == 0:
-        s = 0.1
-        e = 0.2
-    s = e
-    e += 0.1
-print(test)
-t_sort = [0]*10
-# for i in range(100):
-    
+def main():
+    out = int(input())
+    outcount = 0
+    for j in range(5):
+        for i in range(9):
+            s = mem[i][j]
+            base[0] = 1
+            if s[1] == 'O':
+                outcount += 1
+                if outcount == out: return print(point)
+                if outcount % 3 == 0:
+                    # base = [0,0,0,0,0]
+                    
+            elif s[1] == 'B':
+                run(int(s[0]))
+            else:
+                run(4)
+
+main()
 
 
