3981cbffcb321807064e28d10169eee040817fe8
aw52052001 <aw52052001@gmail.com>
2022-05-28 12:58:46 +0800

    master

---
+++ Codingbar/專精班/第13單元.py
@@ -0,0 +1,30 @@
+'''
+密碼檢查器
+'''
+# paw = input()
+
+# for i in range(5):
+#     x = input()
+#     if x != paw:
+#         print("密碼輸入錯誤",(4-i))
+#     else:
+#         print("密碼輸入正確")
+#         break
+# else:
+#     print("此帳戶已被鎖定")
+
+'''
+
+凱薩解密法
+
+
+'''
+eng_letter = "abcdefghijklmnopqrstuvwxyz"
+n = int(input())
+s = input()
+
+
+'''
+
+
+'''
