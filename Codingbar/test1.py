d20b7afcf8a9a1ab7e63e19ce088866799fe531a
aw52052001 <aw52052001@gmail.com>
2022-06-18 13:24:29 +0800

    master

--- Codingbar/test1.py
+++ Codingbar/test1.py
@@ -1,36 +1,22 @@
-mem = []
-for i in range(9):
-    mem.append(input().split()[1:])
-
-base = [0,0,0,0,0]
-
-point = 0
-def run(n):
-    global point
-    for i in range(n):
-        base[1],base[2],base[3],base[4] = base[0],base[1],base[2],base[3]
-        base[0] = 0
-        if base[4]:
-            point += 1;base[4] = 0
-
-def main():
-    out = int(input())
-    outcount = 0
-    for j in range(5):
-        for i in range(9):
-            s = mem[i][j]
-            base[0] = 1
-            if s[1] == 'O':
-                outcount += 1
-                if outcount == out: return print(point)
-                if outcount % 3 == 0:
-                    # base = [0,0,0,0,0]
-                    
-            elif s[1] == 'B':
-                run(int(s[0]))
-            else:
-                run(4)
-
-main()
-
-
+a = '''print()
+ex.
+		print(1)
+		print(“hello”)
+		
+		a = 5
+		print(a)
+		
+	需要印出多個值可以用 , 分開， 會占用一個空格
+	ex.
+		a = 5
+		b = 3
+		print(a,b)'''
+ans = ""
+for i in a:
+    if i == "\t":
+        ans += "\\t"
+    elif i == "\n":
+        ans += "\\n"
+    else:
+        ans += i
+print(ans)
