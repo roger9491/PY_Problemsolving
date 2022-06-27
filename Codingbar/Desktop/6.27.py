c79cf07573ca18a5b3495fb10f59aa11bde73049
aw52052001 <aw52052001@gmail.com>
2022-06-27 17:39:47 +0800

    master

--- Codingbar/Desktop/6.27.py
+++ Codingbar/Desktop/6.27.py
@@ -331,3 +331,171 @@ https://leetcode-cn.com/problems/intersection-of-two-arrays-ii/submissions/
 
 
 # print(ans)
+
+'''
+https://www.luogu.com.cn/problem/P1102
+P1102 A-B 数对
+
+
+# '''
+# X=list(map(int,input().split()))
+# N=X[0]
+# C=X[1]
+# inf=list(map(int,input().split()))
+# dic1={}
+# times=0
+# for i in inf:
+#     if i in dic1:
+#         dic1[i]+=1
+#     else:
+#         dic1[i]=1
+# print(dic1)
+# for i in dic1:
+#     if dic1[i]+C in dic1:
+#         times+=dic1[dic1[i]+C]*dic1[i]
+#         print(dic1[dic1[i]+C]*dic1[i])
+#     else:
+#         continue
+# print(times)
+# n, c = map(int,input().split())
+
+# a = list(map(int,input().split()))
+
+# dic = {}
+
+# for i in a:
+#     if i in dic:
+#         dic[i] += 1
+#     else:
+#         dic[i] = 1
+# # print(dic)
+# ans = 0
+# # print(dic)
+# for i in dic:
+#     if i + c in dic:
+#         ans += dic[i]*dic[i+c]
+#     # if i - c in dic:
+#     #     ans += max(dic[i],dic[i-c])
+
+#     # print(ans)
+# print(ans)
+
+
+'''
+堆疊 
+
+串列
+push
+    a.append()
+
+pop
+    del a[-1]
+    a.pop() 刪除 + 取值
+
+
+c/c++ 陣列 
+先宣告陣列大小
+
+(([])([]))
+
+(([)
+
+配對的時候發生?
+
+'''
+
+# a=input()
+# b=[]
+# c=[]
+# for i in a:
+#     if i in '()/{/}[]':
+#         b.append(i)
+# print(b)
+# for j in range(len(b)):
+#     if b==[]:
+#         break
+#     for i in range(len(b)):
+#         if i==len(b)-1:
+#             break
+#         if b[i]+b[i+1] =="()"or b[i]+b[i+1] =="{}"or b[i]+b[i+1] =="[]":
+#             del b[i]
+#             del b[i+1]
+#             break
+#     print(j,b)
+# if b==[]:
+#     print('True')
+# else:
+#     print("False")
+
+'''
+監聽
+
+'''
+# queue = []
+
+# while True:
+#     n = input()
+#     if n == "no":
+#         break
+
+#     n = int(n)
+#     queue.append(n)
+
+#     while queue[0] < queue[-1] - 86400:
+#         del queue[0]
+
+#     print(len(queue))
+
+
+def suml(a):
+    total = 0
+    for i in a:
+        total += i
+    return total
+a = [[1,2,3],[2,3,4],[4,5,6,4,5,1],[1,2]]
+a.sort(key=suml)
+print(a)
+
+'''
+from functools import cmp_to_key
+
+'''
+
+#  from functools import cmp_to_key
+# def f(a,b):
+#     if int(a+b) < int(b+a):
+#         return -1   #交換 a 和 b
+#     else:
+#         return 1
+
+# n = int(input())
+
+# t = []
+# for i in range(n):
+#     num = input()
+#     t.append(num)
+
+# # for i in range(n):
+# #     for j in range(n-1-i):
+# #         if int(t[j]+t[j+1]) < int(t[j+1] + t[j]):
+# #             temp = t[j]
+# #             t[j] = t[j+1]
+# #             t[j+1] = temp
+# t.sort(reverse=True,key=cmp_to_key(f))
+
+# # 5 5111
+# # 5111 521
+# # 5215111
+# print("".join(t))
+
+'''
+
+作業練習
+
+函數運算式求值
+https://zerojudge.tw/ShowProblem?problemid=f640
+
+砍樹
+https://judge.tcirc.tw/ShowProblem?problemid=d030
+
+'''
