acc15e0fb75fbfdf28d598c3057b51dcf63067c4
roger9491 <aw52052001@gmail.com>
2022-08-08 17:43:26 +0800

    8/8

--- Codingbar/Desktop/8.1.py
+++ Codingbar/Desktop/8.1.py
@@ -4,9 +4,40 @@ bfs 廣度優先搜索
 核心思想 : 擴散
 
 
-dict={0:[2,3],2:[10],3:[4,5],5:[6]}
+dict={0:[2,3],2:[10],3:[4,5],5:[6],8:[9]}
 
 '''
+# dic : 字典 {key:value}
+# if a in dic:
+
+
+# dic={0:[2,3],2:[10],3:[4,5],5:[6],8:[9]}
+
+# queue = [0] #看誰的周圍
+
+# while queue:    #當所有的點看完時，queue就空了
+#     node = queue.pop(0)     #刪除索引值0，並傳出
+#     print(node)
+
+#     if node in dic:
+#         for i in dic[node]:
+#             queue.append(i)
+
+# dic = {0:[2,3],2:[1,10],3:[4,5],5:[6],10:[9,11]}
+# a = [0]
+# while True:
+#     if len(a) == 0:
+#         break
+#     if a[0] in dic:
+#         for i in dic[a[0]]:
+#             a.append(i)
+#     v = a.pop(0)
+#     print(v)
+
+
+
+
+
 # dic = {0:[2,3],2:[1,10],3:[4,5],5:[6],10:[9,11]}
 
 # queue = [0]
@@ -40,6 +71,70 @@ ex
 5 6
 '''
 
+# n = int(input())
+# dic = {}
+# for i in range(n):
+#     c = list(map(int,input().split()))
+#     a = c[0]
+#     b = c[1]
+#     if a in dic:
+#         dic[a].append(b)
+#     else:
+#         dic[a] = [b]
+    
+#     if b in dic:
+#         dic[b].append(a)
+#     else:
+#         dic[b] = [a]
+
+# a = [0]
+# x = []
+# while True:
+#     if len(a) == 0:
+#         break
+#     if a[0] in dic:
+#         for i in dic[a[0]]:
+#             if i not in x:  #判斷i有沒有走過
+#                 a.append(i)
+#     v = a.pop(0)
+#     print(v)
+#     x.append(v)
+
+'''
+優化判斷 i 有沒有走過
+利用索引值取值的方式
+索引值:節點
+值: 有 沒有
+'''
+# n = int(input())
+# dic = {}
+# for i in range(n):
+#     c = list(map(int,input().split()))
+#     a = c[0]
+#     b = c[1]
+#     if a in dic:
+#         dic[a].append(b)
+#     else:
+#         dic[a] = [b]
+    
+#     if b in dic:
+#         dic[b].append(a)
+#     else:
+#         dic[b] = [a]
+
+# a = [0]
+# x = [0]*12
+# while True:
+#     if len(a) == 0:
+#         break
+#     if a[0] in dic:
+#         for i in dic[a[0]]:
+#             if x[i] == 0:  #判斷i有沒有走過
+#                 a.append(i)
+#                 x[i] = 1
+#     v = a.pop(0)
+#     print(v)
+
 
 # n = int(input())
 
@@ -71,39 +166,57 @@ ex
 
 '''
 4.bfs 搜索所有點
+
 '''
 # def bfs(i,j):
-#     queue = [[0,0]]
-#     path = [[0]*4 for i in range(4)]
-#     path[0][0] = 1
-#     while queue:    
-#         node = queue.pop(0) #=> #temp = queue[0]， del queue[0]
-#         print(matrix3[node[0]][node[1]])
-#         for x in dire:
-#             now_i = node[0] + x[0]
-#             now_j = node[1] + x[1]
-#             if 0 <= now_i < 4 and 0 <= now_j < 4:
-#                 if matrix3[now_i][now_j] != "x":
-#                     if path[now_i][now_j] != 1:
-#                         path[now_i][now_j] = 1
-#                         queue.append([now_i,now_j])
+#     queue = [[i,j]]
+#     record = [[0]*4 for i in range(5)]
+#     record[0][0] = 1
+#     dire = [[-1,0], [1,0], [0,-1], [0,1]]
 
-matrix3 = [["s", 1 , 2 ,"x"],
-           [ 3 ,"x", 4 , 5 ],
-           [ 6 , 7 ,"x","x"],
-           [ 8 , 9 , 10 , "E"]]
-# dire = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+#     while queue:
+#         node = queue.pop(0)
+#         print(matrix[node[0]][node[1]])
+#         for x in dire:
+#             i = node[0] + x[0]      #周圍的節點座標
+#             j = node[1] + x[1]
+#             if 0 <= i < len(matrix) and 0 <= j < len(matrix[0]):    #判斷座標合理性
+#                 if record[i][j] == 0 and matrix[i][j] != "x":
+#                     queue.append([i,j])
+#                     record[i][j] = 1
+
+
+# #       上        下      左     右
+# # i,j = [-1,0], [1,0], [0,-1], [0,1]
+# matrix = [ ["s", 1 , 2 ,"x"],
+#            [ 3 ,"x", 4 , 5 ],
+#            [ 6 , 7 , 8 ,"x"],
+#            [ 9 ,"x", 10, 11],
+#            [ 12, 13, 14,'e']]
 # bfs(0,0)
+
+
+
+
+
+
 
 
 '''
 最短距離
-bfs
+兩點之間的最短"距離"
+
+
 
 '''
 
 
 
+matrix = [["s", 1 , 2 ,"x"],
+           [ 3 ,"x", 4 , 5 ],
+           [ 6 , 7 , 8 ,"x"],
+           [ 9 ,"x", 10, 11],
+           [ 12, 13, 14,'e']]
 
 # def bfs(i,j):
 #     queue = [[i,j]]
@@ -317,3 +430,16 @@ dict={0:[2,3],2:[10],3:[4,5],5:[6],2:[10,0]}
 # #       下     右    上     左
 # dir = [[1,0],[0,1],[-1,0],[0,-1]]
 # dfs(0,0)
+
+
+# import sys	#必須先匯入sys
+# for i in sys.stdin: #無限的輸入
+#     n = int(i)
+#     for i in range(n):
+#         a = sys.stdin.readline().strip()
+#         print(a)
+
+
+
+
+
