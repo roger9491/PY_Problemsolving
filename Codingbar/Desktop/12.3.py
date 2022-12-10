23dfc0b6a22bce1a6a2ca2fe1a04216b493134df
roger9491 <aw52052001@gmail.com>
2022-12-10 11:56:49 +0800

    12/3

--- Codingbar/Desktop/12.3.py
+++ Codingbar/Desktop/12.3.py
@@ -1,37 +1,40 @@
+
+
+
+
 '''
 4.bfs 搜索所有點
 
 '''
-def bfs(i, j):
-    queue = [[i, j]]
-    record = [[0]*len(matrix[0]) for i in range(len(matrix))]
+# def bfs(i, j):
+#     queue = [[i, j]]    #準備要查誰的附近節點
+#     record = [[0]*len(matrix[0]) for i in range(len(matrix))]
 
-    while queue:
-        node = queue.pop(0)
-        print(matrix[node[0]][node[1]])
+#     while queue:
+#         node = queue.pop(0)
+#         print(matrix[node[0]][node[1]])
 
-        # 尋找子節點
-        for x in dir:
-            i_next = node[0] + x[0]
-            j_next = node[1] + x[1]
-            # 判斷子節點的座標 在範圍裡
-            if 0 <= i_next < len(matrix) and 0 <= j_next < len(matrix[0]):
-                if matrix[i_next][j_next] != "x" and record[i_next][j_next] == 0:
-                    queue.append([i_next, j_next])
-                    record[i_next][j_next] = 1
+#         # 尋找子節點
+#         for x in dir:
+#             i_next = node[0] + x[0]
+#             j_next = node[1] + x[1]
+#             # 判斷子節點的座標 在範圍裡
+#             if 0 <= i_next < len(matrix) and 0 <= j_next < len(matrix[0]):
+#                 if matrix[i_next][j_next] != "x" and record[i_next][j_next] == 0:
+#                     queue.append([i_next, j_next])
+#                     record[i_next][j_next] = 1
 
-
-
-#       上        下      左     右
-dir = [[-1,0], [1,0], [0,-1], [0,1]]
-matrix = [ ["s", 1 , 2 ,"x"],
-           [ 3 ,"x", 4 , 5 ],
-           [ 6 , 7 , "x" ,"x"],
-           [ 8 , 9 , 10, "e"],]
-bfs(0,0)
+# #       上        下      左     右
+# dir = [[-1,0], [1,0], [0,-1], [0,1]]
+# matrix = [ ["s", 1 , 2 ,"x"],
+#            [ 3 ,"x", 4 , 5 ],
+#            [ 6 , 7 , "x" ,"x"],
+#            [ 8 , 9 , 10, "e"],]
+# bfs(0,0)
 
 
-
+# a = [1,2]
+# print(*a)
 
 
 
@@ -94,9 +97,9 @@ bfs(0,0)
 #     path[0][0] = 1
 #     ans = 0
 #     while queue:
-#         size = len(queue)
+#         size = len(queue)   # 1
 
-#         while size != 0:
+#         while size != 0:    # 前一波的節點找完 步數才能加1
 #             node = queue.pop(0) #s
             
 #             print(matrix[node[0]][node[1]])
@@ -108,7 +111,7 @@ bfs(0,0)
 #                         path[i][j] = 1
 #                         queue.append([i,j])
 #             size -= 1
-#         ans += 1
+#         ans += 1    #步數
 # matrix = [["s", 1 , 2 ,"x"],
 #            [ 3 ,"x", 4 , 5 ],
 #            [ 6 , 7 , 8 ,"x"],
@@ -134,6 +137,8 @@ bfs(0,0)
 d094: Q-7-5. 闖關路線
 https://judge.tcirc.tw/ShowProblem?problemid=d094
 '''
+
+
 '''
 dfs : 深度優先搜尋
 
@@ -149,6 +154,36 @@ dfs : 深度優先搜尋
 遞迴
     函式 呼叫自己
 '''
+
+# def a(c):
+#     c += 1
+#     print(c)
+#     if c > 3:
+#         return
+#     a(c)
+#     print(c)
+# a(0)
+'''
+
+印出 0 ~ 10 間格為2
+
+0
+2
+4
+6
+8
+10
+
+'''
+# def s(n):
+#     print(n)
+#     n+=2
+#     if n>10:
+#         return
+#     s(n)
+# s(0)
+
+
 
 # def a(n):
 #     print(n)
@@ -173,7 +208,35 @@ a(4): 1 + 2 + 3 + 4
 a(4) = a(3) + 4
 a(5) = a(4) + 5
 a(n) = a(n-1) + n 遞迴關係式
+
+
+1 + 2 + 3 + 4 + 5   5階
+1 + 2 + 3 + 4   4階
+
+f(5): 回傳 5階
+f(4): 回傳 4階
+
+f(5) = f(4) + 5
+f(4) = f(3) + 4
+f(3) = f(2) + 3
+f(2) = f(1) + 2
+f(1) = 1
+
+f(n) = f(n-1) + n
+
+if n == 1 return 1
 '''
+# def f(n):   #5
+#     if n == 1:
+#         return 1
+#     return f(n-1) + n
+# print(f(5))
+
+
+
+
+
+
 
 # def a(n):
 #     if n == 1:    #邊界值
@@ -189,17 +252,34 @@ a(n) = a(n-1) + n 遞迴關係式
 '''
 輸入一個數字 n
 用遞迴 1+ ..+n
-
+中間要印出過程
+最後印出 1+ ..+n 結果
 
 5
-a(5) = a(4) + 5
-a(4) = a(3) + 4
-a(3) = a(2) + 3
-a(2) = a(1) + 2
-        1
-a(1) = a(2) + 1
-a(2) = a(3) + 2
+
+1
+3
+6
+10
+15
+15
 '''
+def a(n):
+    if n==1:
+
+        return n
+    c=a(n-1)+n
+    print(c)
+    return c
+
+n=int(input())
+a(n)
+
+
+
+
+
+
 # def a(n):
 #     if n == n1:
 #         return n
